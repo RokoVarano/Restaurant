5a52d5a8b2a988130c9e829897621e0ae8edd0a6
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-21 10:36:56 -0400

    Config_and_setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+console.log("LALALALA");
+
+const mainDiv = document.querySelector('#main');
+
+const subH1 = document.createElement('h1');
+subH1.textContent = 'THIS IS THE SHIT!!';
+
+mainDiv.appendChild(subH1);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index'),
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: "index.js",
+    chunkFilename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        hash: true,
+        filename: './index.html', //relative to root of the application
+        template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      loader: 'babel-loader',
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: 'localhost',
+    port: 8080,
+  }
+};
