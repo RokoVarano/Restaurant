25d52ff3104a2681b3edc13665afce02ed3ee0a5
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-21 19:08:03 -0400

    HTML planning: start

--- src/index.js
+++ src/index.js
@@ -2,14 +2,3 @@ import cuadro1_jpeg from './assets/pictures/cuadro1.jpeg';
 import "./style.scss";
 
 console.log("LALALALA");
-
-const mainDiv = document.querySelector('#main');
-
-const subH1 = document.createElement('h1');
-subH1.textContent = 'THIS IS THE SHIT!!';
-
-const cuadro1 = document.createElement('img');
-cuadro1.src = cuadro1_jpeg;
-
-mainDiv.appendChild(subH1);
-mainDiv.appendChild(cuadro1);
