8663a949b27edae74ee2d446da0e3e1708a850f1
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-21 15:50:27 -0400

    Setup sass support for webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import cuadro1_jpeg from './assets/pictures/cuadro1.jpeg';
+import "./style.scss";
 
 console.log("LALALALA");
 
--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,19 @@ module.exports = {
           }
         }
       ]
-    }]
+    },      
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    },
+  ]
   },
   resolve: {
     extensions: ['.json', '.js', '.jsx']
