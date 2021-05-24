2ec876b04ef19dd10ca337e52f92521cb0c3fba7
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-24 19:52:44 -0400

    Add Text

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const divider = parent => {
   divider.classList.add('divider');
   parent.appendChild(divider);
 }
-
 const body = d.querySelector('body');
 body.classList.add('flex-mid-col');
 
@@ -24,11 +23,13 @@ main.appendChild(header);
 const header_h1 = d.createElement('h1');
 header_h1.id = 'header_h1';
 header_h1.classList.add('flex-mid-col');
+header_h1.textContent = 'Aracne';
 header.appendChild(header_h1);
 
 const header_h3 = d.createElement('h3');
 header_h3.id = 'header_h3';
 header_h3.classList.add('flex-mid-col');
+header_h3.textContent = 'Exotic arthropod cuisine';
 header.appendChild(header_h3);
 
 divider(main);
@@ -46,7 +47,7 @@ services.appendChild(venues);
 
 const venues_h3 = d.createElement('h3')
 venues_h3.id = 'venues_h3';
-venues_h3.classList.add('flex-mid-col');
+venues_h3.textContent = '"Ample, yet intimate dining rooms for couples and family groups."'
 venues.appendChild(venues_h3);
 
 const venues_pic = d.createElement('div');
@@ -62,7 +63,7 @@ services.appendChild(dishes);
 
 const dishes_h3 = d.createElement('h3')
 dishes_h3.id = 'dishes_h3';
-dishes_h3.classList.add('flex-mid-col');
+dishes_h3.textContent = '"Exotic dishes featuring organicly grown insects and bugs, delicately served by international native chefs"';
 dishes.appendChild(dishes_h3);
 
 const dishes_pic = d.createElement('div');
@@ -76,8 +77,6 @@ footer.id = 'footer';
 footer.classList.add('flex-mid-col');
 body.appendChild(footer);
 
-divider(footer);
-
 const address = d.createElement('figure');
 address.id = 'address';
 footer.appendChild(address);
@@ -89,9 +88,8 @@ address.appendChild(add_map);
 
 const add_caption = d.createElement('figcaption');
 add_caption.id = 'add_caption';
+add_caption.textContent = 'Imaginary Street 123, NoWhere City'
 address.appendChild(add_caption);
-
-divider(footer);
 
 const contact = d.createElement('ul');
 contact.id = 'contact';
@@ -99,10 +97,12 @@ footer.appendChild(contact);
 
 const phone = d.createElement('li');
 phone.id = 'phone';
+phone.textContent = 'Phone: +1 7893 8765'
 contact.appendChild(phone);
 
 const email = d.createElement('li');
 email.id = 'email';
+email.textContent = 'Email: aracne@jokemail.com'
 contact.appendChild(email);
 
 
